9566ee58f37811dbdf0a4e3d0470a293dcd8719f
anika <anika@AnikaHPi5.townisp.com>
2021-09-15 00:09:59 -0400

    Added FirstFile information

--- TestProject/src/FirstFile.java
+++ TestProject/src/FirstFile.java
@@ -3,7 +3,17 @@ public class FirstFile {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello!");
+		System.out.println("What form are you in?");
+		System.out.println("Vth Former");
+		
+		System.out.println("Where are you from? Are you a boarder or day student? ");
+		System.out.println("I'm a day student from Shrewsbury, MA");
+		
+		System.out.println("What sports do you play?");
+		System.out.println("I compete in taekwondo and am also on varsity crew");
+		
+		System.out.println("What was your winter sport?");
+		System.out.println("Robotics! I really liked it too :)");
 	}
 	
 }
