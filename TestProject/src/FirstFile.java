384844fc73cbe36f10e20a15d9f5eea974b2c9fc
anika <anika@AnikaHPi5.townisp.com>
2021-09-15 15:12:11 -0400

    Added FirstFile details

--- TestProject/src/FirstFile.java
+++ TestProject/src/FirstFile.java
@@ -10,10 +10,19 @@ public class FirstFile {
 		System.out.println("I'm a day student from Shrewsbury, MA");
 		
 		System.out.println("What sports do you play?");
-		System.out.println("I compete in taekwondo and am also on varsity crew");
+		System.out.println("I compete in taekwondo (outside of school) and am also on varsity crew");
 		
 		System.out.println("What was your winter sport?");
 		System.out.println("Robotics! I really liked it too :)");
+		
+		System.out.println("Where have you lived?");
+		System.out.println("Maryland, NY, Pennsylvania, India, and here");
+		
+		System.out.println("What's your favorite ice cream flavor?");
+		System.out.println("Cookie dough");
+		
+		System.out.println("What's your favorite coding language?");
+		System.out.println("Python (I'm more used to it)");
 	}
 	
 }
